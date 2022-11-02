b06ff19a239f1e94578d15585d7b4441cf52abd1
tubi55 <tubi55@nate.com>
2022-11-02 20:15:30 +0900

    custom className

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,11 +12,11 @@ function Header() {
 				/>
 
 				<ul className='hidden space-x-4 md:flex'>
-					<li>Home</li>
-					<li>TV Shows</li>
-					<li>Movies</li>
-					<li>New & Popular</li>
-					<li>My List</li>
+					<li className='headerLink'>TV Shows</li>
+					<li className='headerLink'>Home</li>
+					<li className='headerLink'>Movies</li>
+					<li className='headerLink'>My List</li>
+					<li className='headerLink'>New & Popular</li>
 				</ul>
 			</div>
 
