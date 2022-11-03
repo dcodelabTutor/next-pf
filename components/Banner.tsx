c91f74a8c3fa38ea694ee10e95484facc8302d45
tubi55 <tubi55@nate.com>
2022-11-03 20:01:18 +0900

    tmdb api key값 발급받고 해당 값을 .env.local파일로 숨김 처리 및 요청 url만들기(NEXT_PUBLIE 접두사)

---
+++ components/Banner.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image';
+//tmdb api (dcodelab , !andy1006)
+
+function Banner() {
+	return (
+		<div>
+			{/* <div className="">
+      <Image />
+    </div> */}
+		</div>
+	);
+}
+
+export default Banner;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+import Banner from '../components/Banner';
 import Header from '../components/Header';
 
 const Home: NextPage = () => {
@@ -14,7 +15,7 @@ const Home: NextPage = () => {
 			<Header />
 
 			<main>
-				{/* Banner */}
+				<Banner />
 				<section></section>
 			</main>
 
