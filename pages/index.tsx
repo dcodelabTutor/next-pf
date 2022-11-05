351c02f385fafd428f0e083753536953deb0b31c
tubi55 <tubi55@nate.com>
2022-11-05 12:58:21 +0900

    파라미터로 전달되는 값 type지정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,36 @@
-import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import Banner from '../components/Banner';
 import Header from '../components/Header';
+import { Movie } from '../typings';
 import requests from '../utils/requests';
 
-const Home: NextPage = () => {
+interface Props {
+	netflixOriginals: Movie[];
+	trendingNow: Movie[];
+	topRated: Movie[];
+	actionMovies: Movie[];
+	comedyMovies: Movie[];
+	horrorMovies: Movie[];
+	romanceMovies: Movie[];
+	documentaries: Movie[];
+}
+
+//아래 getServerSideProps로 리턴된 결과물을 바로 파라미터를 통해 전달받고
+//typings.d.ts파일로 지정된 인터페이스를 다시 위쪽 Props 인터페이스로 지정해서
+//파라미터에 Props 타입 지정
+//이렇게 지정하면 Props로 지정하지 않는 파라미터 입력시 에러출력
+const Home = ({
+	netflixOriginals,
+	actionMovies,
+	comedyMovies,
+	documentaries,
+	horrorMovies,
+	romanceMovies,
+	topRated,
+	trendingNow,
+}: Props) => {
+	console.log(netflixOriginals);
 	return (
 		<div className='relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]'>
 			<Head>
