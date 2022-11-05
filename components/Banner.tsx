9164552feb7f20fa6943588a14cdaf682bb11be4
tubi55 <tubi55@nate.com>
2022-11-05 14:13:49 +0900

    next에서 이미지 컴포넌트 불러오기

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
+import { baseUrl } from '../constants/movie';
 import { Movie } from '../typings';
 
 interface Props {
@@ -7,26 +8,30 @@ interface Props {
 }
 
 function Banner({ netflixOriginals }: Props) {
-	//useState의 초기값에 Movie타입 적용
 	const [movie, setMovie] = useState<Movie | null>(null);
 
 	useEffect(() => {
-		//Math.floor(Math.random()*숫자) (0~숫자 까지의 랜덤수를 반환하는 상용구문)
-		//0~5까지의 랜덤수 반환
-		//console.log(Math.floor(Math.random() * 5));
-
-		//전체 넷플릭스 오리지널 데이터 갯수중에서 컴포넌트 마운트시마다 랜덤 순서의 데이터초기화
 		setMovie(
 			netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)]
 		);
 	}, [netflixOriginals]);
 
-	//넷플릭스의 오리지널 데이터가 변경될때마다 랜덤으로 하나의 데이터를 출력
 	console.log(movie);
 
 	return (
 		<div>
-			<div className=''>{/* <Image src={''} /> */}</div>
+			<div className='absolute top-0 left-0 h-[95vh] w-screen'>
+				<Image
+					src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
+					alt={`${movie?.title}`}
+					//이미지를 넣을땐 width, height값을 모두 넣거나
+					//아니면 layout, objetFit속성을 넣어줘야 됨
+					//width={500}
+					//height={500}
+					layout='fill'
+					objectFit='cover'
+				/>
+			</div>
 		</div>
 	);
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
+  images: {
+    //쌍따옴표로 입력 및 해당 config파일이 바뀌면 무조건 서버 재실행
+    domains: ["image.tmdb.org"],
+  },
 }
