63a3301dbe82c453ba8bab67729138559e2ac9bc
tubi55 <tubi55@nate.com>
2022-11-06 16:43:50 +0900

    썸네일 출력

--- components/Row.tsx
+++ components/Row.tsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/solid';
+import { Movie } from '../typings';
+import Thumbnail from './Thumbnail';
 
-function Row() {
-	return <div>row</div>;
+interface Props {
+	title: string;
+	movies: Movie[];
+}
+
+function Row({ title, movies }: Props) {
+	return (
+		<div className='h-40 space-y-0.5 md:space-y-2'>
+			<h2 className='w-56 cursor-pointer text-sm font-semibold text-[#e5e5e5] transition duration-200 hover:text-white md:text-2xl'>
+				{title}
+			</h2>
+			<div className='group relative md:-ml-2'>
+				{/* group-hover: 부모 group에 호버시.. */}
+				<ChevronLeftIcon className='absolute top-0 bottom-0 left-2 z-40 m-auto h-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100' />
+
+				<div className='flex items-center space-x-0.5 overflow-x-scroll md:space-x-2.5 md:p-2'>
+					{movies.map((movie) => (
+						<Thumbnail key={movie.id} movie={movie} />
+					))}
+				</div>
+
+				<ChevronRightIcon className='absolute top-0 bottom-0 left-2 z-40 m-auto h-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100' />
+			</div>
+		</div>
+	);
 }
 
 export default Row;
--- pages/index.tsx
+++ pages/index.tsx
@@ -40,10 +40,14 @@ const Home = ({
 				<Banner netflixOriginals={netflixOriginals} />
 
 				<section>
-					<Row />
-					<Row />
-					<Row />
-					<Row />
+					<Row title='Trending Now' movies={trendingNow} />
+					<Row title='Top Rated' movies={topRated} />
+					<Row title='Action Thrillers' movies={actionMovies} />
+
+					<Row title='Comedies' movies={comedyMovies} />
+					<Row title='Scary Movies' movies={horrorMovies} />
+					<Row title='Romance Movies' movies={romanceMovies} />
+					<Row title='Documentaries' movies={documentaries} />
 				</section>
 			</main>
 
