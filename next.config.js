90e00beef318ecff2332e1061d88e0df33786d15
tubi55 <tubi55@nate.com>
2022-11-10 21:19:00 +0900

    로그인 페이지 배경 이미지 가져오기, 이미지 호스트 next.config에 추가후 서버 재실행

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@
 module.exports = {
   images: {
     //쌍따옴표로 입력 및 해당 config파일이 바뀌면 무조건 서버 재실행
-    domains: ["image.tmdb.org"],
+    domains: ["image.tmdb.org", "rb.gy"],
   },
 }
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import Head from 'next/head';
+import Image from 'next/image';
 
 function login() {
-	return <div>login</div>;
+	return (
+		<div>
+			<Head>
+				<title>Netflix</title>
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+
+			<Image
+				src='https://rb.gy/p2hphi'
+				fill
+				className='-z-10 !hidden opacity-60 sm:!inline object-cover'
+				alt='logo'
+			/>
+		</div>
+	);
 }
 
 export default login;
