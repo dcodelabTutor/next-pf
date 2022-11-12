b3ee8188a9396dee6f630b297f46766f76b36d4f
tubi55 <tubi55@nate.com>
2022-11-12 14:26:13 +0900

    로그인 폼 완성

--- pages/login.tsx
+++ pages/login.tsx
@@ -22,6 +22,25 @@ function login() {
 				width={150}
 				height={150}
 			/>
+
+			<form className='relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14'>
+				<h1 className='text-4xl font-semibold'>Sign In</h1>
+
+				<div className='space-y-4'>
+					<label htmlFor=""></label>
+					<input type="email" placeholder='Email' className='input' />
+
+					<label htmlFor=""></label>
+					<input type="password" placeholder='Password' className='input' />
+				</div>
+
+				<button className='w-full rounded bg-[#e50914] py-3 font-semibold'>Sign In</button>
+
+				<div className='text-[gray]'>
+					New to Netflix?
+					<button type='submit' className='text-white hover:underline cur'>Sign up now</button>
+				</div>
+			</form>
 		</div>
 	);
 }
