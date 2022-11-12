b5a7debfc7b91652f9e6373b1530919108c189c6
tubi55 <tubi55@nate.com>
2022-11-12 16:20:53 +0900

    form  hook을 이용하여 값을 미 입력시 에러메세지 출력

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,10 +1,34 @@
 //https://react-hook-form.com/get-started
 //npm install react-hook-form
-
+import { signInAnonymously, signInWithPopup } from 'firebase/auth';
 import Head from 'next/head';
 import Image from 'next/image';
+import { useState } from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+
+interface Inputs {
+  email: string;
+  password: string;
+}
 
 function login() {
+  const [Login, setLogin] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<Inputs>();
+
+  // submit이벤트 발생시 등록한 폼에 입력된 값을 콘솔로 출력
+  const onSubmit: SubmitHandler<Inputs> = async ({ email, password }) => {
+    if (Login) {
+      //await signIn(email, passwrd)
+    } else {
+      //await signUp(email, password);
+    }
+  };
+
   return (
     <div className="relative flex h-screen w-screen fex-col bg-black md:items-center md:justify-center md:bg-transparent">
       <Head>
@@ -26,7 +50,9 @@ function login() {
         height={150}
       />
 
-      <form className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14">
+      <form
+        className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14"
+        onSubmit={handleSubmit(onSubmit)}>
         <h1 className="text-4xl font-semibold">Sign In</h1>
 
         <div className="space-y-4">
@@ -35,17 +61,32 @@ function login() {
             type="email"
             placeholder="Email"
             className="input"
+            {...register('email', { required: true })}
           />
+          {/* 해당 항목 미입력시 출력될 에러 구문 */}
+          {errors.email && (
+            <span className="p-1 text-[13px] font-light  text-orange-500">
+              Please enter a valid email
+            </span>
+          )}
 
           <label htmlFor=""></label>
           <input
             type="password"
             placeholder="Password"
             className="input"
+            {...register('password', { required: true })}
           />
+          {errors.password && (
+            <span className="p-1 text-[13px] font-light  text-orange-500">
+              Your password must contain between 4 and 60 characters.
+            </span>
+          )}
         </div>
 
-        <button className="w-full rounded bg-[#e50914] py-3 font-semibold">
+        <button
+          className="w-full rounded bg-[#e50914] py-3 font-semibold"
+          onClick={() => setLogin(true)}>
           Sign In
         </button>
 
@@ -53,7 +94,8 @@ function login() {
           New to Netflix?
           <button
             type="submit"
-            className="text-white hover:underline cur">
+            className="text-white hover:underline cur"
+            onClick={() => setLogin(false)}>
             Sign up now
           </button>
         </div>
